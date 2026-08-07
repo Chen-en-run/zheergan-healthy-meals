605cd1ab0f05dbf47da7b51902cc00b02b120104
Chen <17111434+chenenrun@user.noreply.gitee.com>
2026-08-07 22:46:47 +0800

    图片修改

--- src/main.jsx
+++ src/main.jsx
@@ -858,7 +858,7 @@ function FoodBanner() {
 }
 
 const painPoints = [
-  { keyword: '外卖重油重盐', tag: '饮食困境', desc: '吃完心里难受，罪恶感爆棚', image: '/zheergan-healthy-meals/images/pain-chicken.png' },
+  { keyword: '外卖重油重盐', tag: '饮食困境', desc: '吃完心里难受，罪恶感爆棚', image: '/zheergan-healthy-meals/images/pain-takeout.png' },
   { keyword: '自己做费时费力', tag: '时间成本', desc: '买菜洗切炒刷碗，没时间也没精力', image: '/zheergan-healthy-meals/images/pain-headache.png' },
   { keyword: '减脂餐难吃', tag: '恶性循环', desc: '坚持不过第三天，最后还是炸鸡奶茶兜底', image: '/zheergan-healthy-meals/images/pain-friedchicken.png' },
 ];
